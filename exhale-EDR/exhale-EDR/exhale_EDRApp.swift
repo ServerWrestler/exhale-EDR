b15125e76e1cdbb26ae40c2a42e1df607a5bce4e
ServerWrestler <8258269+ServerWrestler@users.noreply.github.com>
2025-08-22 13:04:26 -0500

    Structured for xcode

---
+++ exhale-EDR/exhale-EDR/exhale_EDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  exhale_EDRApp.swift
+//  exhale-EDR
+//
+//  Created by Eric Chadbourne on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct exhale_EDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
